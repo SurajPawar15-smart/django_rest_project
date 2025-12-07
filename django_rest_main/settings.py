af3a39d431ca85b5fa26acc3ec20a80993219641
SurajPawar15-smart <supersuraj143@gmail.com>
2025-12-07 11:47:52 +0530

    added the add api and tested

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students'
+    'students',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
--- django_rest_main/urls.py
+++ django_rest_main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("students/",include("students.urls"))
+    path("students/",include("students.urls")),
+    
+    # API Endpoints. domainname/api/v1/
+    path('api/v1/', include('api.urls')),
 ]

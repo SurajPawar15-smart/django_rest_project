177275c6fffbfbbe820c433a034a89fc7e1e38d1
SurajPawar15-smart <supersuraj143@gmail.com>
2025-12-07 10:59:05 +0530

    first commit

---
+++ students/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def students(request):
+    students = {'id': 1, 'name': 'John Doe', 'class': 'CS'}
+    return HttpResponse(students['name'])
